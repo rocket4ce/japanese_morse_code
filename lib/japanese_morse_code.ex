232c173a2dad9661a02f7a765a1f6e805f4b1363
Dinko Azema <rocket4ce@gmail.com>
2024-10-12 18:41:17 -0300

    refactor Japanese Morse code mappings and add funtion for encoding

--- lib/japanese_morse_code.ex
+++ lib/japanese_morse_code.ex
@@ -51,14 +51,28 @@ defmodule JapaneseMorseCode do
     "セ" => ".---.",
     "ス" => "---.-",
     "ン" => ".-.-.",
-    "゛" => "..",         # Dakuten
-    "゜" => "..--.",      # Handakuten
-    " " => "/"            # Space represented by "/"
+    # Dakuten
+    "゛" => "..",
+    # Handakuten
+    "゜" => "..--.",
+    # Space represented by "/"
+    " " => "/"
   }
   @doc """
   Return the complete Morse code dictionary for Japanese Kana.
   """
   def morse_code do
     @kana_morse
+  end
+
+  @doc """
+  Encodes a string oj Japanese kana into Morse code.
+  """
+  def encode(text) when is_binary(text) do
+    text
+    |> String.graphemes()
+    |> Enum.map(&Map.get(@kana_morse, &1, ""))
+    |> Enum.filter(&(&1 != ""))
+    |> Enum.join(" ")
   end
 end
